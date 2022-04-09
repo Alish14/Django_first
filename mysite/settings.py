cd1b040692dd5b982fe9360f52ca2c17250c1423
ali <me@gamil.com>
2022-04-09 11:56:04 +0430

    add statics setting and file to project

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,4 +1,4 @@
-
+import os
 """
 Django settings for mysite project.
 
@@ -120,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL ='/static/'
+STATIC_ROOT =BASE_DIR /'static'
+STATICFILES_DIRS = [
+    BASE_DIR /"statics",
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+ 
+ 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- website/views.py
+++ website/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request,'index.html')
 
 def about_me(request):
-    return render(request,'about me.html')
+    return render(request,'about_me.html')
 
 def contact(request):
     return render(request,'contact.html')
