159d64e2f15c8e99396137bfd01c9633182c8121
ali <me@gamil.com>
2022-04-09 23:41:20 +0430

    updated statics settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,11 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL ='/static/'
-STATIC_ROOT =BASE_DIR /'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    BASE_DIR /"statics",
+    BASE_DIR / "statics",
 ]
+ 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
  
