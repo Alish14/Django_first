cf75173f459597dd047ea24a8f139d47f0ec099e
ali <me@gamil.com>
2022-04-12 20:58:33 +0430

    added blog app and its templatest to website

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def blog_view(request):
+    return render(request,'blog/blog-home.html')
+    
+
+def blog_single(request):
+    return render(request,'blog/blog-single.html')
+    
+
+# Create your views here.
