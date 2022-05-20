f1f1d46112a8dc4e910a2e2dbac5221b840130de
ali <me@gamil.com>
2022-05-20 20:29:28 +0430

    change contact for recieve user post request

--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 
 
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
+from website.forms import ContactForm ,Newsletterform
+from django.contrib import messages
 def index(request):
     return render(request,'website/index.html')
 
@@ -10,6 +12,26 @@ def about(request):
     return render(request,'website/about.html')
 
 def contact(request):
-    return render(request,'website/contact.html')
+    if request.method =='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.name="anonymous"
+            post.save()
+            messages.success(request,'your ticket submited successfuly')
+    else:
+        messages.add_message(request,messages.ERROR,"your ticket didn't submited")
+    form=ContactForm()
+    return render(request,'website/contact.html',{'form':form})
+def newsletter_view(request):
+
+    if request.method =='POST':
+        form=Newsletterform(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    else:
+        return HttpResponseRedirect('/')
+
 
 
