4b754f31a924a5a0c3d51a09f7facb55c9b452e5
ali <me@gamil.com>
2022-05-20 20:28:04 +0430

    set up view for contact form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.shortcuts import get_object_or_404, render
-from blog.models import Category, Post
+from blog.models import Post
 from django.db.models import F
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
@@ -21,29 +21,24 @@ def blog_view(request,**kwargs):
     context ={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 def blog_single(request,pid):
-    posts=Post.objects.update(contact_views=F("contact_views")+1)
-    posts=get_object_or_404(Post,pk=pid,status=1)
+    posts=Post.objects.filter(pk=pid).update(contact_views=F("contact_views")+1)
     posts=Paginator(posts,3)
     if Post.objects.filter(pk=pid+1).exists():
         has_next=True
-        
-        
     else:
         has_next=False
     if Post.objects.filter(pk=pid-1).exists():
         has_previous=True
-        
     else:
         has_previous=False
     next_pid=pid+1
     previous_pid=pid-1
+    posts=get_object_or_404(Post,pk=pid,status=1)
     context={'post':posts,'pid':pid,'has_next':has_next,'has_previous':has_previous,'next_pid':next_pid,'previous_pid':previous_pid}
     return render(request,'blog/blog-single.html',context)
 def test(request):
     # posts=Post.objects.filter()
-    
     posts=Post.objects.filter(status=1)
-
     context = {'post':posts}
     return render(request,'test.html',context)
 def blog_category(request,cat_name):
@@ -56,7 +51,6 @@ def blog_search(request):
     if request.method == 'GET':
         if s:=request.GET.get('s'):
             posts=posts.filter(content__contains=s)
-    
     context={'posts':posts}
     return render(request,'blog/blog-home.html',context)
 
@@ -66,6 +60,3 @@ def blog_search(request):
 
 
     
-    
-
-# Create your views here.
